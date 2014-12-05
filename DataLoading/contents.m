eaec01fb67015288150378aabfb8e8705ffa2dd1
elboyran <e.ranguelova@esciencecenter.nl>
2014-12-05 18:11:50 +0100

    Added contents of the DataLoading folder file. Added MATLAB file excange function to read ini files. Reading the metadata for the OCT .bin volume.

--- DataLoading/contents.m
+++ DataLoading/contents.m
@@ -6,4 +6,5 @@
 %--------------------------------------------------------------------------
 %% FUNCTIONS
 %--------------------------------------------------------------------------
-
+%import_oct_csv_file - imports OCT CSV data (2D) from AMC Mitra Almasian
+%import_oct_bin_file - function to load AMC raw volumetric data from file
