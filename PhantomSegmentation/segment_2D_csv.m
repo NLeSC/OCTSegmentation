259ab7bba8b2cf45c929f31c33bf855850561616
elboyran <e.ranguelova@esciencecenter.nl>
2015-01-16 18:19:16 +0100

    some attemptsfor segmenting the 2 CSV phantom data

--- PhantomSegmentation/segment_2D_csv.m
+++ PhantomSegmentation/segment_2D_csv.m
@@ -13,7 +13,7 @@
 %
 % EXAMPLE
 % segment_2D_csv
-% Enter the full filename of the phantom data saved in CSV  format: ../../Data/Phantom/2D_CSV/fantoom0.csv
+% Enter the full filename of the phantom data saved in CSV  format: ..\..\Data\Phantom\2D_CSV\fantoom0.csv
 %
 % REFERENCES
 %
@@ -21,7 +21,53 @@
 %--------------------------------------------------------------------------
 
 %% import the data
-data_file = input('Enter the full filename of the phantom data saved in CSV  format: ', 's');
+%data_file = input('Enter the full filename of the phantom data saved in CSV  format: ', 's');
+data_file = '..\..\Data\Phantom\2D_CSV\fantoom0.csv';
 
 raw_data = import_oct_csv_file_raw(data_file); 
 
+%% parameters file
+%params_file = input('Enter the full filename of the parameters file: ', 's');
+params_file = '.\gui\octseg.param';
+ 
+params_infl = loadParameters('INFL', params_file);
+params_rpe = loadParameters('RPELIN', params_file);
+
+%% segment the borders between the layers
+lines = segment_phantom_lines(raw_data, params_infl, params_rpe);
+
+%% display some steps
+f1 = figure;
+figure(f1);
+
+rows = 2; cols = 4;
+subplot(rows,cols,1); imagesc(raw_data);colormap(gray(256));title('raw data');axis on; grid on
+subplot(rows,cols,2); imagesc(scaled_data);colormap(gray(256));title('scaled data');axis on; grid on
+subplot(rows,cols,3); imagesc(filtered_data);colormap(gray(256));title('filtered\_data');axis on; grid on
+subplot(rows,cols,4); imagesc(thresh_data);colormap(gray(256));title('thersh\_data');axis on; grid on
+[size_x, size_y] = size(raw_data);
+subplot(rows,cols,5); plot(1:size_y,lines(1,:));title('some layer border (max)');axis on, grid on
+axis([0 size_y 0 size_x]);axis ij
+subplot(rows,cols,6); plot(1:size_y,lines(2,:));title('some layer border (min)');axis on, grid on
+axis([0 size_y 0 size_x]);axis ij
+
+
+% f2 = figure;
+% figure(f2);
+% subplot(2,2,1); hist(raw_data(:));title('raw_data');
+% %subplot(2,2,1); hist(data(:));title('data');axis on
+% subplot(2,2,2); hist(scaled_data(:));title('scaled\_data');axis on
+% subplot(2,2,3); hist(filtered_data(:));title('filtered\_data');axis on
+% subplot(2,2,4); hist(thresh_data(:));title('thresh\_data');axis on
+
+% f2 = figure;
+% figure(f2);
+% col=  200;
+% %subplot(2,2,1); plot(raw_data(:,col));title(['raw\_data at column ', num2str(col)]);axis on; grid on;
+% %subplot(2,2,1); hist(data(:));title('data');axis on
+% %subplot(2,2,2); plot(scaled_data(:,col));title(['scaled\_data at column ', num2str(col)]);axis on; grid on;
+% subplot(2,2,1); plot(filtered_data(:,col));title(['filtered\_data at column ', num2str(col)]);axis on; grid on;
+% subplot(2,2,2); plot(thresh_data(:,col));title(['thresh\_data at column ', num2str(col)]);axis on; grid on;
+% subplot(2,2,3); plot(lines(1,:));title('lines');axis on, grid on; axis([0 size_y 0 size_x])
+% %subplot(2,2,4); plot(lines(2,:));title('lines');axis on, grid on
+% %
--- PhantomSegmentation/segment_phantom_lines.m
+++ PhantomSegmentation/segment_phantom_lines.m
@@ -37,29 +37,71 @@
 % e-mail correspondence with Martin and Dirk from AMC
 %
 % NOTES
-function [lines] = segment_phantom_lines(data, PARAMS)
+
+function [lines] = segment_phantom_lines(data, PARAMSI, PARAMSE)
 
 if nargin < 2
-    PARAMS =[];
+    PARAMSI =[];
+    PARAMSE =[];
 end
-%------------------------------------------------------------------------------
-% rescale the data to interval [0,1]
 
+%% rescale the data
+%scaled_data = 10.^(data./20);
 min_value= min(min(data));
 max_value= max(max(data));
 
-data = (data - min_value)./(max_value - min_value);
+scaled_data = (data - min_value)./(max_value - min_value);
+
+%scaled_data = sqrt(scaled_data);
+scaled_data(scaled_data>1) = 0;
 
 % make some data available for inspection in the work space
-assignin('base', 'data', data);
+assignin('base', 'scaled_data', scaled_data);
 
-%------------------------------------------------------------------------------
-% take sqrt of he data
-data(data>1) = 0;
-data = sqrt(data);
+%% filter the data with median filterring
+mask = PARAMSI.INFLLIN_VOLUME_MEDFILT;
+mask = mask ./ sum(mask);
 
-% make some data available for inspection in the work space
-assignin('base', 'data', data);
+%filtered_data = imfilter(scaled_data, mask, 'symmetric') ;
 
+filtered_data = medfilt2(scaled_data,[13 17]);
+assignin('base', 'filtered_data', filtered_data);
 
-lines=[];
+%% threshold the data
+thresh_data = treshold(filtered_data, 'ascanmax', [0.97 0.5]);
+%thresh_data = imadjust(thresh_data);
+%thresh_data = medfilt2(thresh_data,[3 5]);
+assignin('base', 'thresh_data', thresh_data);
+
+%% results
+%automatic
+line_max = findRetinaExtrema(thresh_data, PARAMSI, 1, 'max');
+line_min = findRetinaExtrema(thresh_data, PARAMSI, 1, 'min');
+% % ransac
+line_max_Ransac = ransacEstimate(line_max, 'poly', ...
+                            PARAMSE.RPELIN_RANSAC_NORM_RPE, ...
+                            PARAMSE.RPELIN_RANSAC_MAXITER, ...
+                            PARAMSE.RPELIN_RANSAC_POLYNUMBER);
+                        
+line_min_Ransac = ransacEstimate(line_min, 'poly', ...
+                            PARAMSE.RPELIN_RANSAC_NORM_RPE, ...
+                            PARAMSE.RPELIN_RANSAC_MAXITER, ...
+                            PARAMSE.RPELIN_RANSAC_POLYNUMBER);
+
+% merge
+lmax = mergeLines(line_max, line_max_Ransac, 'discardOutliers', [PARAMSE.RPELIN_MERGE_THRESHOLD ...
+                                                           PARAMSE.RPELIN_MERGE_DILATE ...
+                                                           PARAMSE.RPELIN_MERGE_BORDER]);
+
+lmin = mergeLines(line_min, line_min_Ransac, 'discardOutliers', [PARAMSE.RPELIN_MERGE_THRESHOLD ...
+                                                           PARAMSE.RPELIN_MERGE_DILATE ...
+                                                           PARAMSE.RPELIN_MERGE_BORDER]);
+% lmax = line_max;
+% lmin = line_min;
+% smooth
+line_max =  linesweeter(lmax, PARAMSI.INFL_SEGMENT_LINESWEETER_FINAL);
+line_min =  linesweeter(lmin, PARAMSI.INFL_SEGMENT_LINESWEETER_FINAL);
+
+lines(1,:) = line_max;
+lines(2,:) = line_min;
+assignin('base', 'lines', lines);
